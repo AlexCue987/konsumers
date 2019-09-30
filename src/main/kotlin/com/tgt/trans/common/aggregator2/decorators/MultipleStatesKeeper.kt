35fca201ec9cc5d5b8b4c6c6626eee2666b4ea82
Alexander.Kuznetsov <z002w5y@target.com>
2019-09-30 14:08:41 -0500

    ChainedMultipleStatesKeeperTest.kt

--- src/main/kotlin/com/tgt/trans/common/aggregator2/decorators/MultipleStatesKeeper.kt
+++ src/main/kotlin/com/tgt/trans/common/aggregator2/decorators/MultipleStatesKeeper.kt
@@ -3,9 +3,8 @@ package com.tgt.trans.common.aggregator2.decorators
 import com.tgt.trans.common.aggregator2.consumers.Consumer
 import com.tgt.trans.common.aggregator2.consumers.ConsumerBuilder
 
-class MultipleStatesKeeper<T>(private val innerConsumer: Consumer<T>, val states: List<Consumer<T>>): Consumer<T> {
-    constructor(innerConsumer: Consumer<T>,  vararg statesArgs: Consumer<T>) : this(innerConsumer, statesArgs.toList())
-
+class MultipleStatesKeeper<T>(private val innerConsumer: Consumer<T>,
+                              private val states: List<Consumer<T>>): Consumer<T> {
     override fun process(value: T) {
         innerConsumer.process(value)
         states.forEach { it.process(value) }
@@ -24,11 +23,13 @@ class MultipleStatesKeeperBuilder<T>(vararg statesArgs: Consumer<T>): ConsumerBu
 
 fun<T> keepStates(vararg statesArgs: Consumer<T>) = MultipleStatesKeeperBuilder(*statesArgs)
 
-//class ChainedStateKeeperBuilder<T, V>(private val previousBuilder: ConsumerBuilder<T, V>,
-//                                      private val stateToKeep: Consumer<V>): ConsumerBuilder<T, V> {
-//    override fun build(innerConsumer: Consumer<V>): Consumer<T> =
-//        previousBuilder.build(StateKeeper(innerConsumer, stateToKeep))
-//}
-//
-//fun<T, V> ConsumerBuilder<T, V>.keepState(stateToKeep: Consumer<V>) =
-//    ChainedStateKeeperBuilder(this, stateToKeep)
+class ChainedMultipleStatesKeeperBuilder<T, V>(private val previousBuilder: ConsumerBuilder<T, V>,
+                                               vararg statesArgs: Consumer<V>): ConsumerBuilder<T, V> {
+    private val statesToKeep = statesArgs.toList()
+
+    override fun build(innerConsumer: Consumer<V>): Consumer<T> =
+        previousBuilder.build(MultipleStatesKeeper(innerConsumer, statesToKeep))
+}
+
+fun<T, V> ConsumerBuilder<T, V>.keepStates(vararg statesArgs: Consumer<V>) =
+    ChainedMultipleStatesKeeperBuilder(this, *statesArgs)
---
+++ src/test/kotlin/unit/com/tgt/trans/common/aggregator2/decorators/ChainedMultipleStatesKeeperTest.kt
@@ -0,0 +1,45 @@
+package com.tgt.trans.common.aggregator2.decorators
+
+import com.tgt.trans.common.aggregator2.consumers.asList
+import com.tgt.trans.common.aggregator2.consumers.consume
+import com.tgt.trans.common.aggregator2.consumers.counter
+import org.junit.jupiter.api.assertAll
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+class ChainedMultipleStatesKeeperTest {
+    private val listToKeep = asList<Int>()
+    private val countToKeep = counter<Int>()
+    private val sut = filterOn<Int> { it > 0 }.keepStates(listToKeep, countToKeep).asList()
+
+    @Test
+    fun `handles no items`() {
+        assertAll(
+            { assertTrue { listToKeep.results().isEmpty() } },
+            { assertEquals(0L, countToKeep.results()) }
+        )
+    }
+
+    @Test
+    fun `handles one item`() {
+        val value = 42
+        sut.process(value)
+
+        assertAll(
+            { assertEquals (listOf(value), listToKeep.results()) },
+            { assertEquals(1L, countToKeep.results()) }
+        )
+    }
+
+    @Test
+    fun `handles several items`() {
+        val items = listOf(42, 43)
+        items.consume(sut)
+
+        assertAll(
+            { assertEquals (items, listToKeep.results()) },
+            { assertEquals(2L, countToKeep.results()) }
+        )
+    }
+}
