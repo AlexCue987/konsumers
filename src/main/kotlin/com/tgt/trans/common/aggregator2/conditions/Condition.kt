f31d4fcfa50af65d656b6bfb62f80e5da1104b15
Alexander.Kuznetsov <z002w5y@target.com>
2019-10-01 14:16:18 -0500

    fix-test

--- src/main/kotlin/com/tgt/trans/common/aggregator2/conditions/Condition.kt
+++ src/main/kotlin/com/tgt/trans/common/aggregator2/conditions/Condition.kt
@@ -5,10 +5,6 @@ interface Condition<T> {
     fun emptyCopy(): Condition<T>
 }
 
-interface StatefulCondition<T> {
-    fun accepted(value: T): Boolean
-}
-
 interface State<T, V> {
     fun process(value: T)
     fun stateValue(): V
