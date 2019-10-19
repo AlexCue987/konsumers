b3d27bcc936b9ee88a997c007a05bbf23be389e1
Alexander.Kuznetsov <z002w5y@target.com>
2019-10-19 12:58:40 -0700

    StateKeeperTest.kt-passes-stop-call

--- src/test/kotlin/unit/com/tgt/trans/common/konsumers/transformations/StateKeeperTest.kt
+++ src/test/kotlin/unit/com/tgt/trans/common/konsumers/transformations/StateKeeperTest.kt
@@ -2,6 +2,8 @@ package com.tgt.trans.common.konsumers.transformations
 
 import com.tgt.trans.common.konsumers.consumers.asList
 import com.tgt.trans.common.konsumers.consumers.consume
+import com.tgt.trans.common.konsumers.dispatchers.allOf
+import com.tgt.trans.common.testutils.FakeStopTester
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
@@ -27,5 +29,14 @@ class StateKeeperTest {
         val items = listOf(42, 43)
         items.consume(sut)
         assertEquals (items, stateToKeep.results())
+    }
+
+    @Test
+    fun `passes stop call downstream`() {
+        val fakeStopTester = FakeStopTester<Int>()
+        val sut = keepState(stateToKeep).allOf(fakeStopTester)
+        sut.stop()
+
+        assertTrue(fakeStopTester.isStopped())
     }
 }
